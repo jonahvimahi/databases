d448b8570b3d8c8c79a5a9968a95e3182b904eff
jonahvimahi <jonahvimahi@gmail.com>
2021-08-13 11:04:43 -0600

    added sql extra credit

--- sql-skills.sql
+++ sql-skills.sql
@@ -27,18 +27,18 @@ WHERE billing_country = 'USA'
 
 SELECT *
 FROM invoice 
-ORDER BY total DESC
+ORDER BY total DESC;
 
 SELECT *
 FROM invoice 
-ORDER BY total ASC
+ORDER BY total ASC;
 
 SELECT *
 FROM invoice 
-WHERE total > 5
+WHERE total > 5;
 
 SELECT SUM(total)
-FROM invoice
+FROM invoice;
 
 -- JOIN QUERIES
 SELECT *
@@ -50,7 +50,7 @@ WHERE unit_price > .99;
 SELECT i.invoice_date, c.first_name, c.last_name, total
 FROM invoice i
 JOIN customer c
-ON i.customer_id = c.customer_id
+ON i.customer_id = c.customer_id;
 
 SELECT c.first_name, c.last_name, e.first_name, e.last_name
 FROM customer c
@@ -60,4 +60,69 @@ ON c.support_rep_id = e.employee_id;
 SELECT album.title, artist.name
 FROM album
 JOIN artist
-ON album.artist_id = artist.artist_id
+ON album.artist_id = artist.artist_id;
+
+-- EXTRA CREDIT 
+
+        -- ARTIST TABLE
+SELECT * 
+FROM artist
+ORDER BY name DESC
+LIMIT 10;
+
+SELECT * 
+FROM artist
+WHERE name LIKE 'Black%';
+
+SELECT * 
+FROM artist
+WHERE name LIKE '%Black%';
+
+        -- EMPLOYEE TABLE
+
+SELECT birth_date
+FROM employee
+ORDER BY birth_date DESC;
+
+SELECT birth_date
+FROM employee
+ORDER BY birth_date ASC;
+
+        -- INVOICE TABLE
+
+SELECT COUNT (*)
+FROM invoice 
+WHERE billing_state IN('CA', 'TX', 'AZ');
+
+SELECT AVG(total)
+FROM invoice;
+
+        -- MORE JOIN QUERIES
+
+SELECT * 
+FROM playlist_track pt
+WHERE playlist_id IN(
+    SELECT playlist_id
+    FROM playlist
+    where name = 'Music'
+);
+
+SELECT name
+FROM track t
+WHERE track_id IN(
+  SELECT track_id
+  FROM playlist_track
+  WHERE playlist_id = 5
+);
+
+SELECT t.name, p.name
+FROM track t
+JOIN playlist_track pt ON t.track_id = pt.track_id
+JOIN playlist p ON pt.playlist_id = p.playlist_id;
+
+SELECT t.name, alb.title
+FROM track t
+JOIN album alb ON t.album_id = alb.album_id
+JOIN artist art ON alb.artist_id = art.artist_id
+
+
